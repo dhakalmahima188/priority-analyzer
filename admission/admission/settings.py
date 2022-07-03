b7eef31c5612af59eeb15fa9fb9fc9d3165d21c6
mahima <076bct033.mahima@pcampus.edu.np>
2022-07-03 16:25:28 +0545

    Integrated django and react

--- admission/admission/settings.py
+++ admission/admission/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR,'frontend/public')
+           # os.path.join(BASE_DIR,'frontend/public')
             ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -155,8 +155,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/public/static')]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'frontend/src/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
